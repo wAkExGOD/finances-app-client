5bf44099f1ef2b67f95f83a455e7a5f29028ffef
Vlad Panasik <vladpanasik@mail.ru>
2025-05-18 02:33:37 +0300

    feat: add meta tags

--- app/(withAppLayout)/purchases/page.tsx
+++ app/(withAppLayout)/purchases/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next"
 import { Purchases } from "@/components/pages"
+
+export const metadata: Metadata = {
+  title: "Finances App - Purchases",
+  description: "Add, update and delete purchases in Finance App",
+}
 
 export default function Page() {
   return <Purchases />
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next"
 import { Auth } from "@/components/pages"
+
+export const metadata: Metadata = {
+  title: "Finances App - Auth",
+}
 
 export default function Page() {
   return <Auth />
